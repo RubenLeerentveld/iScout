1ad7d7d14f7a25aa7f7a0131bb3d145cb2c11bae
unknown <Ruben@Leerentveld.net>
2022-07-31 13:17:01 +0200

    initial commit

---
+++ src/main/java/com/rubenleerentveld/iscout/IScoutApplication.java
@@ -0,0 +1,13 @@
+package com.rubenleerentveld.iscout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IScoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IScoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rubenleerentveld/iscout/IScoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubenleerentveld.iscout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IScoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
